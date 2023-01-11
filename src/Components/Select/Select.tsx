292f214c9f28725798ab50c000f2a9bdd6b60935
Vadim <vadim515.vp@gmail.com>
2023-01-11 23:59:18 +0500

    add select

--- src/Components/Select/Select.tsx
+++ src/Components/Select/Select.tsx
@@ -1,38 +1,29 @@
-// import classNames from 'classnames'
-// import styles from './Select.module.scss'
+import classNames from 'classnames'
+import { useState } from 'react'
+import styles from './Select.module.scss'
+import arrowForSelect from 'assets/img/arrowForSelect.png'
 const Select = () => {
-//   const [showOptions, setShowOptions] = useState(false)
-//   const hasOptions = options.length > 0
+  const [showOptions, setShowOptions] = useState(false)
+  const hasOptions = true
 
-//   return (
-//     <div className={classNames([styles.select])}>
-//     <button
-//       type={'button'}
-//       className={classNames([
-//         // props.btnClass,
-//         styles.currentValue,
-//         {
-//           [styles.placeholder]: value === undefined,
-//           [styles.valueError]: props.error !== undefined,
-//           [styles.showOptions]: showOptions,
-//         },
-//       ])}
-//       onClick={() => setShowOptions((v) => hasOptions && !v)}>
-//       {value?.label ?? 'Select...'}
-//     </button>
-//     <div className={classNames(styles.arrow, { [styles.arrowOpen]: showOptions })}>
-//       <SVG.Arrow style={{ fill: 'var(--color_placeholder)', strokeWidth: 1 }} />
-//     </div>
-//     {props.error && <span className={styles.error}>{props.error}</span>}
-//     {hasOptions && (
-//       <div className={classNames(styles.options, { [styles.optionsOpen]: showOptions })}>
-//         {options.map((e) => (
-//           <button key={e.value} type={'button'} className={styles.option} onClick={() => onChange(e)}>
-//             {e.label}
-//           </button>
-//         ))}
-//       </div>
-//   )
+  return (
+    <div className={classNames([styles.select])} onClick={() => setShowOptions((v) => hasOptions && !v)}>
+      <div className={classNames(styles.arrow, { [styles.arrowOpen]: showOptions })}>
+        {/* <SVG.Arrow style={{ fill: 'var(--color_placeholder)', strokeWidth: 1 }} /> */}
+        <img src={arrowForSelect} alt="" />
+      </div>
+      {/* {props.error && <span className={styles.error}>{props.error}</span>} */}
+      {hasOptions && (
+        <div className={classNames(styles.options, { [styles.optionsOpen]: showOptions })}>
+          {/* {options.map((e) => (
+            <button key={e.value} type={'button'} className={styles.option} onClick={() => onChange(e)}>
+              {e.label}
+            </button>
+          ))} */}
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Select
