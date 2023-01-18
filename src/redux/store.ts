9c4d2ed21cd0a080de77da822314ca99b22d7e05
Vadim <vadim515.vp@gmail.com>
2023-01-18 16:35:03 +0500

    micro fix img in bookpage

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,20 +1,39 @@
-import { TypedUseSelectorHook, useDispatch as useReduxDispatch, useSelector as useReduxSelector } from 'react-redux';
-import { applyMiddleware, combineReducers, createStore, compose, Action } from 'redux';
-import  thunkMiddleWare, { ThunkAction } from 'redux-thunk'
-import bookReducer from './bookReducer';
+import {
+  TypedUseSelectorHook,
+  useDispatch as useReduxDispatch,
+  useSelector as useReduxSelector,
+} from "react-redux";
+import {
+  applyMiddleware,
+  combineReducers,
+  createStore,
+  compose,
+  Action,
+} from "redux";
+import thunkMiddleWare, { ThunkAction } from "redux-thunk";
+import bookReducer from "./bookReducer";
 let rootReducer = combineReducers({
-    bookReducer: bookReducer,
+  bookReducer: bookReducer,
 });
 
-type RootReducerType = typeof rootReducer
+type RootReducerType = typeof rootReducer;
 
-export type RootState = ReturnType<typeof rootReducer>
+export type RootState = ReturnType<typeof rootReducer>;
 
-export type AppStateType = ReturnType<RootReducerType> 
+export type AppStateType = ReturnType<RootReducerType>;
 
-export type InferActionsTypes<T> = T extends  {[key:string]: (...args: any[]) => infer U}? U : never
+export type InferActionsTypes<T> = T extends {
+  [key: string]: (...args: any[]) => infer U;
+}
+  ? U
+  : never;
 
-export type BaseThunkType<A extends Action, R = Promise<void>> = ThunkAction<R, AppStateType, unknown, A>
+export type BaseThunkType<A extends Action, R = Promise<void>> = ThunkAction<
+  R,
+  AppStateType,
+  unknown,
+  A
+>;
 
 //  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -22,22 +41,17 @@ export type BaseThunkType<A extends Action, R = Promise<void>> = ThunkAction<R, 
 
 // const store = createStore(rootReducer, undefined, composedEnhancers)
 
-export const useSelector: TypedUseSelectorHook<RootState> = useReduxSelector
+export const useSelector: TypedUseSelectorHook<RootState> = useReduxSelector;
 
-export type AppDispatch = typeof store.dispatch
-export const useDispatch = () => useReduxDispatch<AppDispatch>()
-
+export type AppDispatch = typeof store.dispatch;
+export const useDispatch = () => useReduxDispatch<AppDispatch>();
 
 // @ts-ignore
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunkMiddleWare)));
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunkMiddleWare))
+);
 
 //  window.__store__ = store;
 export default store;
-
-
-
-
-
-
-
